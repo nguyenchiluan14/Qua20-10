651fa1314e2ca9bb9ffe47bfbf830647928e5ec2
Nguyễn Chí Luân <nguyenluan01042005@gmail.com>
2024-10-20 21:37:50 +0700

    Add files via upload

---
+++ 20_10.js
@@ -0,0 +1,4 @@
+document.querySelector('.flower-button').addEventListener('click', function() {
+    const flower = document.querySelector('.flower-animation');
+    flower.style.transform = 'scale(1)';
+});
